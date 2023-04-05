025ade53ce1e035102c0b230771d95cf37260c8e
Nagamatsu Ryosuke <61650874+NagatsukiSep@users.noreply.github.com>
2023-04-05 16:05:48 +0900

    fix

--- scripts/ZBot.js
+++ scripts/ZBot.js
@@ -3,4 +3,9 @@ module.exports = robot => {
     robot.respond(/hoge$/i, res => {
       res.reply("fuga");
     });
+
+    // "@botName hoge fuga"を受け取ったら"@senderName fuga"を送り返す
+    robot.respond(/hoge (.+)$/i, res => {
+        res.reply(res.match[1]);
+        });
   };
